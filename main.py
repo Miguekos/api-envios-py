55556c65cb916d39b7d03ca9560bfb6328876495
Miguekos <miguel.rodriguez@acceso.com.pe>
2020-06-16 02:17:31 -0500

    upload config and dockerfile - 2

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ async def app_shutdown():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9776, log_level="info", reload=True)
 #
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", loop="asyncio")
--- readme.md
+++ readme.md
@@ -4,3 +4,5 @@ uvicorn main:app --host="0.0.0.0" --log-level="info"  --reload
 
 
 docker build -t api_envios:1.0 .
+
+
